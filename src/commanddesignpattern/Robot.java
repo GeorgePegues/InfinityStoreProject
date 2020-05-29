e213eb26a5f18bc4574c3adb9253be98641b802c
wzhang640 <wzhang640@users.noreply.github.com>
2020-05-29 19:31:12 -0400

    add documentation to robot

--- src/commanddesignpattern/Robot.java
+++ src/commanddesignpattern/Robot.java
@@ -1,5 +1,9 @@
 package commanddesignpattern;
 
+/**
+ * Class for Robot; contains four commands
+ * @author William Zhang
+ */
 public class Robot {
 
     private String name;
@@ -8,18 +12,30 @@ public class Robot {
         this.name = name;
     }
 
+    /**
+     * Robot picks up treasure
+     */
     public void pickup() {
         System.out.println("Oh treasure, picking it up!");
     }
 
+    /**
+     * Robot jumps
+     */
     public void jump() {
         System.out.println("Oh no a hazard, I'm jumping over it.");
     }
 
+    /**
+     * Robot sprays fire
+     */
     public void fire() {
         System.out.println("Bad guys! Fire my blow torch at them.");
     }
 
+    /**
+     * Robot heals
+     */
     public void heal() {
         System.out.println("Thanks for healing my wounds.");
     }

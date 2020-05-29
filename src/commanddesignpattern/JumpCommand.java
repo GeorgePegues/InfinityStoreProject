4937fb7ecc6a06f653ba27e1bed4c3b9a113d6a2
ashmonnatiya <ashmon.natiya@gmail.com>
2020-05-29 19:09:08 -0400

    JumpCommand

---
+++ src/commanddesignpattern/JumpCommand.java
@@ -0,0 +1,24 @@
+/**
+ * this class is responsible to the Jump Command it allows for the robot to jump
+ * @author natiyaashmon
+ */
+public class JumpCommand implements Command {
+	private Robot robot;
+	/**
+	 * this is the jump command it tells the robot to jump
+	 * @param robot the robot
+	 */
+	public JumpCommand (Robot robot) {
+		this.robot = robot;
+	}
+	/**
+	 * this method tells the robot to jump
+	 */
+	public void execute () {
+		robot.jump();
+
+
+	}
+
+
+}

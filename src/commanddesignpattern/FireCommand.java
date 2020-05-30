79b9b356cd0127105bf3050980c8685a5f15418a
GeorgePegues <61563338+GeorgePegues@users.noreply.github.com>
2020-05-30 11:12:34 -0400

    Add files via upload
    Uploaded Jump and fire command

---
+++ src/commanddesignpattern/FireCommand.java
@@ -0,0 +1,20 @@
+package decoratordesignpattern;
+
+public class FireCommand {
+	Robot robot;
+	
+	public FireCommand(Robot robot)
+	{
+		this.robot = Robot;
+	}
+	
+	public void execute()
+	{
+		if(key.equalsIgnoreCase("fire"))
+		{
+		System.out.println("Bad guys! Fire my blow torch at them. ");
+		}
+	}
+
+
+}
--- src/commanddesignpattern/JumpCommand.java
+++ src/commanddesignpattern/JumpCommand.java
@@ -1,25 +1,20 @@
-package commanddesignpattern;
-/**
- * this class is responsible to the Jump Command it allows for the robot to jump
- * @author natiyaashmon
- */
-public class JumpCommand implements Command {
-	private Robot robot;
-	/**
-	 * this is the jump command it tells the robot to jump
-	 * @param robot the robot
-	 */
-	public JumpCommand (Robot robot) {
-		this.robot = robot;
+package decoratordesignpattern;
+
+public class JumpCommand {
+	
+	Robot robot;
+	
+	public JumpCommand(Robot robot)
+	{
+		this.robot = Robot;
 	}
-	/**
-	 * this method tells the robot to jump
-	 */
-	public void execute () {
-		robot.jump();
-
-
+	
+	public void execute()
+	{
+		if(key.equalsIgnoreCase("jump"))
+		{
+		System.out.println("Oh no a hazard, I'm jumping over it.");
+		}
 	}
-
 
 }
